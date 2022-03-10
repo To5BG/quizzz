f669892775187f2c8092258a42f2a8f61cec1bb5
Beni Selyem <b.selyem@student.tudelft.nl>
2022-03-10 19:40:35 +0100

    fix: Fix checkstyle issues
    Remove unused imports

--- server/src/test/java/server/api/TestActivityRepository.java
+++ server/src/test/java/server/api/TestActivityRepository.java
@@ -1,17 +1,7 @@
 package server.api;
 
 import commons.Activity;
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery;
 import server.database.ActivityRepository;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
 
 public class TestActivityRepository extends StubRepository<Activity, Long> implements ActivityRepository {
     public TestActivityRepository() {
--- server/src/test/java/server/api/TestGameSessionRepository.java
+++ server/src/test/java/server/api/TestGameSessionRepository.java
@@ -15,17 +15,6 @@
  */
 package server.api;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.function.Function;
-
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
-
 import commons.GameSession;
 import server.database.SessionRepository;
 
