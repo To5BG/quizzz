6af5ca4bac79d774013f37e404b9ed2dcdc50173
Beni Selyem <b.selyem@student.tudelft.nl>
2022-03-10 14:29:30 +0100

    refactor: Fix checkstyle issues
    Remove unused imports

--- server/src/test/java/server/api/LeaderboardControllerTest.java
+++ server/src/test/java/server/api/LeaderboardControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.util.Optional;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.*;
