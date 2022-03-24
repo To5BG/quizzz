f573d9f77d43353b4cc8d4e46b58288067acd9b3
Rithik Appachi Senthilkumar <r.appachisenthilkumar@student.tudelft.nl>
2022-03-24 12:07:38 +0100

    fix: estimation points bug

--- client/src/main/java/client/scenes/GameCtrl.java
+++ client/src/main/java/client/scenes/GameCtrl.java
@@ -489,13 +489,17 @@ public abstract class GameCtrl implements Initializable {
                 } catch (NumberFormatException ex) {
                     givenAnswer = 0;
                 }
-                int diff = Math.abs(givenAnswer - actualAnswer);
+                // diff : magnitude of difference between the entered answer and the actual answer
+                double diff = Math.abs(givenAnswer - actualAnswer);
                 if (diff == 0) {
                     temppoints = (int) (60 * this.evaluation.points * timeFactor) + 40;
                 } else {
                     if (diff > actualAnswer) diff = actualAnswer;
-                    temppoints = (int) (90 - 90 * ((double) diff * difficultyFactor * timeFactor / actualAnswer) +
-                            ((diff < actualAnswer) ? 10 - 10 * ((double) diff * difficultyFactor / actualAnswer) : 0));
+                    // timeDependent : the part of the points that depends on time and accuracy - between 0 and 90
+                    double timeDependent = (90 - 90 * (diff * difficultyFactor / actualAnswer)) * timeFactor;
+                    // constant : the part of the points that depends on accuracy and not time - between 0 and 10
+                    double constant = (diff < actualAnswer) ? 10 - 10 * (diff * difficultyFactor / actualAnswer) : 0;
+                    temppoints = (int) (timeDependent + constant);
                     if (temppoints <= 0) temppoints = 0;
                 }
                 break;
