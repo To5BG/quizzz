ef34120e1753a2baadeacea58304873fef300946
Yongcheng Huang <Y.Huang-51@student.tudelft.nl>
2022-03-27 16:52:11 +0200

    Delete the useless override method in Joker Record
    Delete useless method since the record will generate is automatically.

--- commons/src/main/java/commons/Joker.java
+++ commons/src/main/java/commons/Joker.java
@@ -3,16 +3,5 @@ package commons;
 public record Joker(
         String username,
         String jokerName
-){
-
-    @Override
-    public String jokerName() {
-        return jokerName;
-    }
-
-    @Override
-    public String username() {
-        return username;
-    }
-}
+){ }
 
