54f5c6fdb12075991676dededabc568b712d39a4
Rithik Appachi Senthilkumar <r.appachisenthilkumar@student.tudelft.nl>
2022-03-27 15:50:20 +0200

    removed unused imports

--- server/src/main/java/server/api/ActivityController.java
+++ server/src/main/java/server/api/ActivityController.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
